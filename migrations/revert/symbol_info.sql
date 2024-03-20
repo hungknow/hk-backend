971218edfbc2b9d34941ca191f141b4bbb3d642e
hungbuiknow <hungbuiknow@gmail.com>
2024-03-20 15:13:07 +0700

    Add symbol_info

---
+++ migrations/revert/symbol_info.sql
@@ -0,0 +1,7 @@
+-- Revert hk_backend:symbol_info from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ migrations/verify/symbol_info.sql
@@ -0,0 +1,7 @@
+-- Verify hk_backend:symbol_info on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
